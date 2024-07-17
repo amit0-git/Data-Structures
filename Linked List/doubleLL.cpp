f7ab8acc6b8be598634a3eb536e28ee5f5ddddad
Amit Verma <root.avanti@gmail.com>
2024-07-17 23:20:50 +0530

    Dll delete node

--- Linked List/doubleLL.cpp
+++ Linked List/doubleLL.cpp
@@ -89,27 +89,30 @@ Node *deleteHead(Node *head)
     return head;
 }
 
-
 // 2. delete tail node
 
-Node *deleteTail(Node *head){
+Node *deleteTail(Node *head)
+{
 
-    if (head==nullptr){
+    if (head == nullptr)
+    {
         return head;
     }
-    // if there is one element 
+    // if there is one element
 
-    if (head->next==nullptr){
+    if (head->next == nullptr)
+    {
         delete head;
         return nullptr;
     }
 
-    Node *temp=head;
-    while(temp->next!=nullptr){
-        temp=temp->next;
+    Node *temp = head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
     }
-    Node *newTail=temp->prev;
-    newTail->next=nullptr;
+    Node *newTail = temp->prev;
+    newTail->next = nullptr;
 
     delete temp;
 
@@ -118,17 +121,132 @@ Node *deleteTail(Node *head){
 
 // 3.  delete kth element
 
+//  1 2 3 4 5
+Node *deleteKth(Node *head, int pos)
+{
+    if (head == nullptr)
+    {
+        return nullptr; // If the list is empty, return nullptr
+    }
 
+    int c = 0;
+    Node *tmp = head;
 
+    // Traverse to the k-th node
+    while (tmp != nullptr)
+    {
+        c++;
+        if (c == pos)
+        {
+            break;
+        }
+        tmp = tmp->next;
+    }
 
+    // If pos is greater than the length of the list, return head
+    if (tmp == nullptr)
+    {
+        return head;
+    }
+
+    Node *back = tmp->prev;
+    Node *front = tmp->next;
+
+    // Case 1: If there is only one element
+    if (back == nullptr && front == nullptr)
+    {
+        delete tmp;
+        return nullptr;
+    }
+    // Case 2: Deleting the first element
+    else if (back == nullptr)
+    {
+        head = head->next;
+        head->prev = nullptr;
+        delete tmp;
+        return head;
+    }
+    // Case 3: Deleting the last element
+    else if (front == nullptr)
+    {
+        back->next = nullptr;
+        delete tmp;
+        return head;
+    }
+    // Case 4: Deleting a middle element
+    else
+    {
+        back->next = front;
+        front->prev = back;
+        delete tmp;
+    }
+
+    return head;
+}
+
+// 4. delete K node
+
+Node *deleteNode(Node *head, int element)
+{
+
+    if (head == nullptr)
+    {
+        return nullptr;
+    }
+
+    Node *tmp = head;
+    while (tmp != nullptr)
+    {
+
+        if (tmp->data == element)
+        {
+            break;
+        }
+
+        tmp = tmp->next;
+    }
+
+    // if the element not found
+    if (tmp == nullptr)
+    {
+        return head;
+    }
+    // if the node to be deleted is the head node
+
+    if (head == tmp)
+    {
+        head = tmp->next;
+
+        if (head != nullptr)
+        {
+            head->prev = nullptr;
+        }
+    }
+
+    // if the node to be deleted is not the head node
+
+    else
+    {
+        tmp->prev->next = tmp->next;
+
+        if (tmp->next != nullptr)
+        {
+            tmp->next->prev = tmp->prev;
+        }
+    }
+    // delete the temp node
+    delete tmp;
+
+    return head;
+}
 int main()
 {
 
-    vi v = {1,2,3};
+    vi v = {1, 2, 3};
     Node *head = convertArr2Dll(v);
     print(head);
 
-    head = deleteTail(head);
+    head = deleteNode(head, 4);
     print(head);
 
     return 0;
